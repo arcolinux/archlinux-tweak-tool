aff87e5788fd82e7c45611a72349d4e76f08e621
Brad Heffernan <ph3onix83@gmail.com>
2020-01-30 11:57:18 +1030

    testing service

--- usr/share/arcolinux-tweak-tool/GUI.py
+++ usr/share/arcolinux-tweak-tool/GUI.py
@@ -140,7 +140,7 @@ def GUI(self, Gtk, Gdk, GdkPixbuf, base_dir, os):
     Functions.get_icon_themes(self, self.iconCombo)
     Functions.get_cursor_themes(self, self.cursorCombo)
 
-    self.cursor_size.set_value(int(Functions.get_gtk_settings(self, "gtk-cursor-theme-size").split(".")[0]))
+    self.cursor_size.set_value(int(str(Functions.get_gtk_settings(self, "gtk-cursor-theme-size")).split(".")[0]))
     self.fonts.set_font_name(Functions.get_gtk_settings(self, "gtk-font-name"))
     
 
