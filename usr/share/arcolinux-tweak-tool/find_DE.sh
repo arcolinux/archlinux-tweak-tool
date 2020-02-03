2f1d9048120df67e814a7ce93be7b74a3d9fb81e
Brad Heffernan <ph3onix83@gmail.com>
2020-02-04 07:53:25 +1030

    updated

--- usr/share/arcolinux-tweak-tool/find_DE.sh
+++ usr/share/arcolinux-tweak-tool/find_DE.sh
@@ -29,20 +29,20 @@ PS=`pgrep -U "${USR}"`
 
 # Get the values of $XDG_CURRENT_DESKTOP, $GDMSESSION, $DESKTOP_SESSION from each "/proc/$ProcessNumber/environ" file
 for PN in $PS; do
-        XDG_CURRENT_DESKTOP+=$($SUDO sed -zne 's/^XDG_CURRENT_DESKTOP=//p' "/proc/$PN/environ" 2>/dev/null; echo " ")
+        DESKTOP_SESSION+=$($SUDO sed -zne 's/^DESKTOP_SESSION=//p' "/proc/$PN/environ" 2>/dev/null; echo " ")
         # GDMSESSION+=$($SUDO sed -zne 's/^GDMSESSION=//p' "/proc/$PN/environ" 2>/dev/null; echo " ")
         # DESKTOP_SESSION+=$($SUDO sed -zne 's/^DESKTOP_SESSION=//p' "/proc/$PN/environ" 2>/dev/null; echo " ")
 done
 
 # Get the most frequent name of any desctop environment
 # This is a way to find the current DE when it is changed a little bit ago
-XDG_CURRENT_DESKTOP=$(echo -e ${XDG_CURRENT_DESKTOP[@]} | get_frequent)
+DESKTOP_SESSION=$(echo -e ${DESKTOP_SESSION[@]} | get_frequent)
 # GDMSESSION=$(echo -e ${GDMSESSION[@]} | get_frequent)
 # DESKTOP_SESSION=$(echo -e ${DESKTOP_SESSION[@]} | get_frequent)
 
 # Print the output values
 # if [ "$OUTPUT" == "simple" ]; then
-echo "${XDG_CURRENT_DESKTOP[@],,}" | sed 's/\-.*//'
+echo "${DESKTOP_SESSION[@],,}" | sed 's/\-.*//'
 # else
 #         echo "user: $(id -n -u $USR)"
 #         echo "uid:  $USR"
