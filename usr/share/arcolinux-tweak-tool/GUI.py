f884b58f8bbac85896fe33d688904e1a71890fb6
Brad Heffernan <ph3onix83@gmail.com>
2020-02-16 19:04:29 +1030

    version

--- usr/share/arcolinux-tweak-tool/GUI.py
+++ usr/share/arcolinux-tweak-tool/GUI.py
@@ -195,7 +195,7 @@ def GUI(self, Gtk, Gdk, GdkPixbuf, base_dir, os):
 #                       VERSION
     # =====================================================
     version = Gtk.Label(xalign=0)
-    version.set_markup("<span foreground=\'grey\'>v20.3.23</span>")
+    version.set_markup("<span foreground=\'grey\'>v20.3.24</span>")
 
     self.lbl_desktop = Gtk.Label(xalign=0)
     self.lbl_desktop.set_markup("<span foreground=\'grey\'>" + self.desktop +"</span>")
