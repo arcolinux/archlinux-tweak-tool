f69cfd70bc9f322ce8383e19063555af1d3df497
Brad Heffernan <ph3onix83@gmail.com>
2020-02-19 13:54:08 +1030

    updated

--- usr/share/arcolinux-tweak-tool/Splash.py
+++ usr/share/arcolinux-tweak-tool/Splash.py
@@ -1,7 +1,7 @@
 import gi
 from Functions import os
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GdkPixbuf  # noqa
+from gi.repository import Gtk, GdkPixbuf, Gdk  # noqa
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -18,10 +18,24 @@ class splashScreen():
         self.window.add(main_vbox)
 
         self.image = Gtk.Image()
+        self.image.set_name('wind')
         pimage = GdkPixbuf.Pixbuf().new_from_file_at_size(base_dir +
                                                           "/images/splash.png",
                                                           400, 200)
         self.image.set_from_pixbuf(pimage)
 
         main_vbox.pack_start(self.image, True, True, 0)
+
+        css = b"""
+#wind {
+    border: 6px solid #196966;
+}"""
+        style_provider = Gtk.CssProvider()
+        style_provider.load_from_data(css)
+
+        Gtk.StyleContext.add_provider_for_screen(
+            Gdk.Screen.get_default(),
+            style_provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
         self.window.show_all()
