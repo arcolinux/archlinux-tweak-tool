ca7cffa36d95c0ae50e2807c606591ade844b117
Brad Heffernan <ph3onix83@gmail.com>
2020-02-24 07:53:14 +1030

    updated termite

--- usr/share/arcolinux-tweak-tool/termite.py
+++ usr/share/arcolinux-tweak-tool/termite.py
@@ -2,7 +2,7 @@
 # =                  Author: Brad Heffernan                       =
 # =================================================================
 import Functions
-import numpy as np 
+import numpy as np
 import Settings
 from Functions import os
 # ====================================================================
@@ -10,35 +10,41 @@ from Functions import os
 # ====================================================================
 
 
-def get_themes(combo):
+def get_themes(combo):  # noqa
     if os.path.isdir(Functions.home + "/.config/termite/themes/"):
         themes = os.listdir(Functions.home + "/.config/termite/themes/")
         combo.get_model().clear()
         with open(Functions.termite_config, "r") as f:
             lines = f.readlines()
             f.close()
-        
-        theme_line = lines[Functions._get_position(lines, "[colors]") + 1]
-        active = ""
-        coms = []
-        for theme in themes:
-            if ".config" in theme:
-                # print(theme.replace("base16-", "").replace(".config", ""))
-                if theme.replace("base16-", "").replace(".config", "").capitalize() in theme_line:
-                    active = theme.replace(".config", "")
-                coms.append(theme.replace(".config", ""))
-        
-        coms.sort()
 
-        if Functions.os.path.isfile(Functions.config):
-            themes = Settings.read_settings("TERMITE", "theme")
-            if len(themes) > 1:
-                active = themes
+        try:
+            theme_line = lines[Functions._get_position(lines, "[colors]") + 1]
 
-        for i in range(len(coms)):
-            combo.append_text(coms[i])
-            if active == coms[i]:
-                combo.set_active(i)
+            active = ""
+            coms = []
+            for theme in themes:
+                if ".config" in theme:
+                    if theme.replace("base16-",
+                                     "").replace(".config",
+                                                 "").capitalize() in theme_line:
+                        active = theme.replace(".config", "")
+                    coms.append(theme.replace(".config", ""))
+
+            coms.sort()
+
+            if Functions.os.path.isfile(Functions.config):
+                themes = Settings.read_settings("TERMITE", "theme")
+                if len(themes) > 1:
+                    active = themes
+
+            for i in range(len(coms)):
+                combo.append_text(coms[i])
+                if active == coms[i]:
+                    combo.set_active(i)
+        except:  # noqa
+
+            pass
 
     # print(lines[Functions._get_position(lines, "[colors]") + 1])
 
@@ -51,7 +57,7 @@ def get_config():
         target_element = "[colors]\n"
         try:
             target_index = lists.index(target_element)
-        except:
+        except:  # noqa
             return lists
 
         return lists[:target_index]
@@ -60,28 +66,33 @@ def get_config():
 
 def set_config(self, theme):
     if not os.path.isfile(Functions.termite_config + ".bak"):
-        Functions.shutil.copy(Functions.termite_config, Functions.termite_config + ".bak")
+        Functions.shutil.copy(Functions.termite_config,
+                              Functions.termite_config + ".bak")
 
     try:
         config = get_config()
 
-        with open(Functions.home + "/.config/termite/themes/" + theme + ".config", "r") as f:
+        with open(Functions.home +
+                  "/.config/termite/themes/" +
+                  theme +
+                  ".config", "r") as f:
             theme_list = f.readlines()
             f.close()
 
         configs = list(np.append(config, theme_list))
 
-        if not configs == None:
+        if configs is not None:
             with open(Functions.termite_config, "w") as f:
                 f.writelines(list(configs))
                 f.close()
 
-            # Functions.MessageBox(self, "Success!!", "Settings Saved Successfully")
-            Functions.show_in_app_notification(self, "Settings Saved Successfully")
+            Functions.show_in_app_notification(self,
+                                               "Settings Saved Successfully")
         if Functions.os.path.isfile(Functions.config):
             Settings.write_settings("TERMITE", "theme", theme)
 
-    except:
-        Functions.MessageBox(self, "Error!!", "Something went wrong setting this theme.")
-
-
+    except Exception as e:
+        print(e)
+        Functions.MessageBox(self,
+                             "Error!!",
+                             "Something went wrong setting this theme.")
