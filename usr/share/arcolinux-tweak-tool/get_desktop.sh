3841c01254ee19dbc75dee6faebb4abcd8261cea
Brad Heffernan <ph3onix83@gmail.com>
2020-03-04 03:53:16 +1030

    updated

--- usr/share/arcolinux-tweak-tool/get_desktop.sh
+++ usr/share/arcolinux-tweak-tool/get_desktop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#======================================================
+#           Author:  alexeevdv 
+#======================================================
+
 function detect_gnome()
 {
     ps -e | grep -E '^.* gnome-session$' > /dev/null
