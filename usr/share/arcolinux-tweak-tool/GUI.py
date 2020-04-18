60b2555775fbe75ab12cf37c37fe3b94d02a0659
Erik Dubois <erik.dubois@gmail.com>
2020-04-18 14:12:37 +0200

    20.5.9

--- usr/share/arcolinux-tweak-tool/GUI.py
+++ usr/share/arcolinux-tweak-tool/GUI.py
@@ -244,7 +244,7 @@ def GUI(self, Gtk, Gdk, GdkPixbuf, base_dir, os):  # noqa
     #                       VERSION
     # =====================================================
     version = Gtk.Label(xalign=0)
-    version.set_markup("<span foreground=\'grey\'>v20.4.26</span>")
+    version.set_markup("<span foreground=\'grey\'>v20.5-9</span>")
 
     self.lbl_desktop = Gtk.Label(xalign=0)
     self.lbl_desktop.set_markup("<span foreground=\'grey\'>" +
