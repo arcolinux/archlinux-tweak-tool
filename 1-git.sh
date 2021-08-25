685cc177c4fbfbe7a43192229ca70c24d952afbb
Erik Dubois <erik.dubois@gmail.com>
2021-08-25 18:42:49 +0200

    /etc/pacman.conf is there too

--- 1-git.sh
+++ 1-git.sh
@@ -36,6 +36,10 @@ sed -i "s/$FIND/$REPLACE/g" $workdir/usr/local/share/arcolinux/sddm.conf.d/kde_s
 echo "getting latest arcolinux-mirrorlist"
 wget https://raw.githubusercontent.com/arcolinux/arcolinux-mirrorlist/master/etc/pacman.d/arcolinux-mirrorlist -O $workdir/usr/local/share/arcolinux/arcolinux-mirrorlist
 
+echo "getting latest /etc/pacman.conf"
+wget https://raw.githubusercontent.com/arcolinux/arcolinuxl-iso/master/archiso/airootfs/etc/pacman.conf -O $workdir/usr/local/share/arcolinux/pacman.conf
+
+
 # Below command will backup everything inside the project folder
 git add --all .
 
