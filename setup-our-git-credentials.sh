f5c159eebeb84175d09bc919d44b4d75ca60befa
arcolinuxz <arcolinuxinfo@gmail.com>
2022-07-18 20:53:38 +0200

    test push

--- setup-our-git-credentials.sh
+++ setup-our-git-credentials.sh
@@ -59,69 +59,12 @@ echo "--------------------------------------------------------------------------
 echo "this is project https://github.com/$githubdir/$project"
 echo "-----------------------------------------------------------------------------"
 
-echo
-tput setaf 1
-echo "################################################################"
-echo "#####  Choose wisely - one time setup after clean install   ####"
-echo "################################################################"
-tput sgr0
-echo
-echo "Select the correct desktop"
-echo
-echo "0.  Do nothing"
-echo "1.  Erik"
-echo "2.  Raniel"
-echo "3.  Steve"
-echo "Type the number..."
-
-read CHOICE
-
-case $CHOICE in
-
-    0 )
-      echo
-      echo "########################################"
-      echo "We did nothing as per your request"
-      echo "########################################"
-      echo
-      ;;
-
-    1 )
-			git config --global pull.rebase false
-			git config --global push.default simple
-			git config --global user.name "arcolinuxz"
-			git config --global user.email "arcolinuxinfo@gmail.com"
-			sudo git config --system core.editor nano
-			#git config --global credential.helper cache
-			#git config --global credential.helper 'cache --timeout=32000'
-      git remote set-url origin git@github.com-arc:$githubdir/$project
-      echo
-      echo "Everything set"
-      ;;
-    2 )
-			git config --global pull.rebase false
-			git config --global push.default simple
-			git config --global user.name "Raniel Laguna"
-			git config --global user.email "avraniel@gmail.com"
-			sudo git config --system core.editor nano
-			git config --global credential.helper cache
-			git config --global credential.helper 'cache --timeout=32000'
-      ;;
-    3 )
-			git config --global pull.rebase false
-			git config --global push.default simple
-			git config --global user.name "Steve Younger"
-			git config --global user.email "coritanie@gmail.com"
-			sudo git config --system core.editor nano
-			git config --global credential.helper cache
-			git config --global credential.helper 'cache --timeout=32000'
-      ;;
-    * )
-      echo "#################################"
-      echo "Choose the correct number"
-      echo "#################################"
-      ;;
-esac
+git config --global pull.rebase false
+git config --global push.default simple
+git config --global user.name "arcolinuxz"
+git config --global user.email "arcolinuxinfo@gmail.com"
+sudo git config --system core.editor nano
+git remote set-url origin git@github.com-arc:$githubdir/$project
 
 echo
 tput setaf 3
