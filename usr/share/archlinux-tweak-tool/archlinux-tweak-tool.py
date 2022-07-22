f5fa22de47afcb3930a47d3a1c9ea26aa4ca211a
arcolinuxz <arcolinuxinfo@gmail.com>
2022-07-22 08:41:49 +0200

    autologin for arch

--- usr/share/archlinux-tweak-tool/archlinux-tweak-tool.py
+++ usr/share/archlinux-tweak-tool/archlinux-tweak-tool.py
@@ -3541,6 +3541,7 @@ class Main(Gtk.Window):
     # ====================================================================
 
     def on_click_user_apply(self, widget):
+        fn.add_autologin_group(self)
         user.create_user(self)
         user.pop_cbt_users(self, self.cbt_users)
 
