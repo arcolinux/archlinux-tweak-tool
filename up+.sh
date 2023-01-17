c1fbe9e57d20cfa48f44fb07649c3125c98962c1
arcolinuxz <arcolinuxinfo@gmail.com>
2023-01-17 16:57:17 +0100

    update

--- up+.sh
+++ up+.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
-set -e
+#set -e
 ##################################################################################################################
-# Author 	: 	Erik Dubois
-# Website 	: 	https://www.erikdubois.be
-# Website	:	https://www.arcolinux.info
-# Website	:	https://www.arcolinux.com
-# Website	:	https://www.arcolinuxd.com
-# Website	:	https://www.arcolinuxforum.com
+# Author    : Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
+# Website   : https://www.ariser.eu
+# Website   : https://www.arcolinux.info
+# Website   : https://www.arcolinux.com
+# Website   : https://www.arcolinuxd.com
+# Website   : https://www.arcolinuxb.com
+# Website   : https://www.arcolinuxiso.com
+# Website   : https://www.arcolinuxforum.com
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
+
+# reset - commit your changes or stash them before you merge
+# git reset --hard - personal alias - grh
 
 # checking if I have the latest files from github
 echo "Checking for newer files online first"
@@ -138,8 +155,15 @@ git commit -m "$input"
 
 # Push the local files to github
 
-git push -u origin master
+if grep -q main .git/config; then
+	echo "Using main"
+		git push -u origin main
+fi
 
+if grep -q master .git/config; then
+	echo "Using master"
+		git push -u origin master
+fi
 
 echo "################################################################"
 echo "###################    Git Push Done      ######################"
